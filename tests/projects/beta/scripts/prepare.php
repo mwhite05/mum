8829f0c3835428a28ff745d2f61704033358a2e5
Aron Duby <aron@binaryevolution.com>
2019-07-02 12:14:37 -0400

    add a comment about env vars and php

--- tests/projects/beta/scripts/prepare.php
+++ tests/projects/beta/scripts/prepare.php
@@ -2,5 +2,9 @@
 <?php
 
 # Do your custom work here
+/**
+ * To access the mum variables as $_ENV make sure your php.ini settings for `variables_order` has E at the beginning.
+ * Alternatively, you can use the `getenv` function or pull them from the $_SERVER super variable
+ */
 $env = print_r($_ENV, true);
 file_put_contents('/tmp/envdump.txt', $env);
